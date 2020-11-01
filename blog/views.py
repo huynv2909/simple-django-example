d27b81a99a329d3963d6343d59ef474199f84be4
Huy Nguyen <vhuy.nguyen@sift-ag.com>
2020-11-01 15:30:47 +0700

    upload old Django sample project to github

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+
+    return render(request, 'index.html', {"posts": posts})
+
+
+def post(request, slug):
+    return render(None, 'post.html', {
+        'post': get_object_or_404(Post, slug=slug)
+    })
+
+
+def about(request):
+    return render(request, 'about.html', {})
